45e00a8d09a36f964e6c3f136549e92c85d196c5
giovannidw <giovannidwidjosewojo@hotmail.com>
2019-04-02 16:21:23 +0200

    fix login

--- server.js
+++ server.js
@@ -1,84 +1,130 @@
+const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser')
 const multer = require('multer')
 const mongo = require('mongodb')
+const mongoose = require('mongoose')
 const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+// const passportLocalMongoose = require('passport-local-mongoose')
+// const slug = require('slug')
+
+
 
 require('dotenv').config()
 
 var db = null
 var url = 'mongodb://' + process.env.DB_HOST + ':' + process.env.DB_PORT
 
-mongo.MongoClient.connect(url, function (err, client) {
-    if (err) throw err
-    db = client.db(process.env.DB_NAME)
-})
+// mongo.MongoClient.connect(url, function (err, client) {
+//     if (err) throw err
+//     db = client.db(process.env.DB_NAME)
+// })
+mongoose.Promise = global.Promise;
+mongoose.connect(url, function (err, client) {
+                if (err) throw err
+                db = client.db(process.env.DB_NAME)
+            })
+    .then(() => console.log('connection succesful'))
+    .catch((err) => console.error(err))
+    
+var index = require('./app/routes/index')
+var users = require('./app/routes/users')
+
+
 
 var upload = multer({
-    dest: 'static/uploads/'
+    dest: './app/static/uploads/'
 })
-const express = require('express')
-const app = express()
-const port = 3000
 
+const port = 3000
+const app = express()
     app.set('view engine', 'ejs')
-    app.set('views', path.join(__dirname, 'views'))
+    app.set('views', path.join(__dirname, 'app/views'))
+    app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended: true}))
-    app.use(express.static(path.join(__dirname, '/static')))
+    app.use(express.static(path.join(__dirname, 'app/static')))
+    app.use(session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+    
+
+app.use('/', index);
+app.use('/users', users);
+
+
+const User = require('./app/models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
     app.get('/chat', chat)
-    app.get('/', renderHome)
-    app.get('/sign-up', renderSignup)
-    app.get('/login', renderLogin)
+    // app.get('/', renderHome)
+    // app.get('/register', renderRegister)
+    // app.get('/login', renderLogin)
     app.get('/profile', profile)
-    app.post('/sign-up', signup)
-    app.post('/login', login)
+    // app.post('/sign-up', register)
+    // // app.post('/login', login)
+    // app.post('/login', passport.authenticate('local', {
+    //     successRedirect: '/profile',
+    //     failureRedirect: '/login'
+    // }))
     app.post('/profile', upload.single('cover'), addGame)
     app.delete('/profile:id', removeGame)
     app.use(function (req, res, next) {
         res.locals.user = null
         next()
     })
-    app.use(session({
-        resave: false,
-        saveUninitialized: true,
-        secret: process.env.SESSION_SECRET
-    }))
+    
     app.get('*', error)
     app.listen(port)
 
-function renderHome(req, res) {
-    res.render('pages/index')
-}
+
+
+
+
+// function renderHome(req, res) {
+//     res.render('pages/index')
+// }
 function chat(req, res) {
     res.render('pages/chat')
 }
-function renderLogin(req, res) {
-    res.render('pages/login')
-}
-function renderSignup(req, res) {
-    res.render('pages/sign-up')
-}
+// function renderLogin(req, res) {
+//     res.render('pages/login')
+// }
+// function renderRegister(req, res) {
+//     res.render('pages/sign-up')
+// }
 function profile(req, res, next) {
-    db.collection('game').find().toArray(done)
+   db.collection('game').find().toArray(done)
     function done(err, game) {
         if (err) {
             next(err)
         } else {
             res.render('pages/profile', {
+                
                 game: game
             })
         }
     }
 }
 function addGame(req, res, next) {
-// var id = slug(req.body.title).toLowerCase()
 
-    // if (!req.session.user) {
-    //     res.status(401).send('Credentials required')
-    //     return
-    // } else {
-    //     res.redirect('/sign-up')
-    // }
+db.collection('game').find().toArray(done)
+
+    if (!req.session.user) {
+        res.status(401).send('Credentials required')
+        return
+    } else {
+        res.redirect('/sign-up')
+    }
     db.collection('game').insertOne({
         title: req.body.title,
         cover: req.file ? req.file.filename : null,
@@ -113,48 +159,66 @@ function removeGame(req, res, next) {
         }
     }   
 }
-function signup(req, res, next) {
 
-    db.collection('user').insertOne({
-        username: req.body.username,
-        password: req.body.password,
-        console: req.body.description
-    }, done)
+// function register(req, res) {
+// User.register(new User({
+//     username: req.body.username
+// }), req.body.password, function (err, user) {
+//     if (err) {
+//         console.log(err);
+//         return res.redirect('sign-up');
+//     } else {
+//         passport.authenticate('local')(req, res, function () {
+//             res.redirect('/profile');
+//         });
+//     }
+// });
+// }
 
-    function done(err) {
-        if (err) {
-            next(err)
-        } else {
-            res.redirect('/profile')
-        }
-    }
-}
-function login(req, res) {
-    var username = req.body.username
-    var password = req.body.password
 
-    db.collection('user').findOne({
-        username: username,
-        password: password
 
-    }, done)
+// function signup(req, res, next) {
 
-    function done(err, user) {
-        if (user) {
-                req.session.user = user
-                res.render('pages/login', {
-                    id: user._id,
-                    username: user.username
-                })
+//     db.collection('user').insertOne({
+//         username: req.body.username,
+//         password: req.body.password,
+//         console: req.body.description
+//     }, done)
+
+//     function done(err) {
+//         if (err) {
+//             next(err)
+//         } else {
+//             res.redirect('/profile')
+//         }
+//     }
+// }
+// function login(req, res) {
+//     var username = req.body.username
+//     var password = req.body.password
+
+//     db.collection('user').findOne({
+//         username: username,
+//         password: password
+
+//     }, done)
+
+//     function done(err, user) {
+//         if (user) {
+//                 req.session.user = user
+//                 res.render('pages/login', {
+//                     id: user._id,
+//                     username: user.username
+//                 })
         
-        } else {
-            res.json({
-                status: 'ok'
-            })
-            res.status(401).send('Password incorrect')
-        }
-    } 
-}
+//         } else {
+//             res.json({
+//                 status: 'ok'
+//             })
+//             res.status(401).send('Password incorrect')
+//         }
+//     } 
+// }
 
 function error(req, res) {
     res.render('static' + req.url, function (err, html) {
